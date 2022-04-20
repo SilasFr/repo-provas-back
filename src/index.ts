d4ece522f67d0cdf6d1d3209c43515dce5abd6f8
SilasFr <silas23.fr@gmail.com>
2022-04-20 00:52:12 -0300

    docs: create error handler middleware

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
 import dotenv from "dotenv";
+import { ErrorHandler } from "./middlewares/errorHandler.js";
 dotenv.config();
 
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(ErrorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`running on ${process.env.PORT}`);
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,3 @@
+import { NextFunction, Request, Response } from "express";
+
+export function ErrorHandler(req: Request, res: Response, next: NextFunction) {}
